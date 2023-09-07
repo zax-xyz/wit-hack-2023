bc992460e979f0f079622f4636d3ad1e4445ebb3
Michael Vo <zax@zaxu.xyz>
2023-09-08 00:12:42 +1000

    feat: scarcity levels

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -7,7 +7,7 @@ import {
   MapPinIcon,
   UserCircleIcon,
 } from "@heroicons/react/24/outline";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import MapGl, {
   type GeoJSONSourceRaw,
   Layer,
@@ -73,16 +73,56 @@ const Map = ({
   useHydrateAtoms([[loggedInAtom, isAuthenticated]]);
   const [loggedIn, setLoggedIn] = useAtom(loggedInAtom);
 
+  const [selectedCouncil, setSelectedCouncil] = useState(0);
+
   const [showPopup, setShowPopup] = useState(false);
   const [popupCoords, setPopupCoords] = useState({ longitude: 0, latitude: 0 });
   const [popupTitle, setPopupTitle] = useState("");
 
   const { data: { LocalGovernmentArea: LGAs } = {}, isLoading: lgasLoading } =
     api.data.getLGAs.useQuery();
-  const {
-    data: { LocalGovernmentArea: LGAsRed } = {},
-    isLoading: lgasRedLoading,
-  } = api.data.getLGAsRed.useQuery();
+  const { data: scarcityLevels, isLoading: scarcityLevelsLoading } =
+    api.data.getScarcityLevels.useQuery();
+  // const {
+  //   data: { LocalGovernmentArea: LGAsRed } = {},
+  //   isLoading: lgasRedLoading,
+  // } = api.data.getLGAsRed.useQuery();
+
+  const sortedLGAs = useMemo(() => {
+    if (!scarcityLevels || !LGAs) {
+      return LGAs;
+    }
+    return {
+      ...LGAs,
+      features: LGAs.features.sort(
+        (a, b) =>
+          scarcityLevels[a.properties.councilname]! -
+          scarcityLevels[b.properties.councilname]!
+      ),
+    };
+  }, [LGAs, scarcityLevels]);
+
+  const LGAsNeedsMet = useMemo(
+    () => ({
+      ...LGAs,
+      features: LGAs?.features.filter((f) => {
+        const scarcity = scarcityLevels?.[f.properties.councilname];
+        return scarcity !== undefined && scarcity >= 3.5;
+      }),
+    }),
+    [LGAs, scarcityLevels]
+  );
+
+  const LGAsNeedsNotMet = useMemo(
+    () => ({
+      ...LGAs,
+      features: LGAs?.features.filter((f) => {
+        const scarcity = scarcityLevels?.[f.properties.councilname];
+        return scarcity !== undefined && scarcity < 3.5;
+      }),
+    }),
+    [LGAs, scarcityLevels]
+  );
 
   const { mutate: logout } = api.user.logout.useMutation({
     onSuccess: () => {
@@ -112,7 +152,7 @@ const Map = ({
                   <ArrowRightOnRectangleIcon tw="w-6 h-6" />
                 </Button>
               ) : (
-                <Button as={Link} href="/auth">
+                <Button as={Link} href="/auth?next=/map">
                   Login
                 </Button>
               )}
@@ -155,7 +195,7 @@ const Map = ({
       <div tw="flex h-full min-h-0">
         <div tw="flex flex-col flex-shrink-0 w-96 h-full overflow-y-auto shadow-md z-10 bg-white">
           <h2 tw="text-xl p-4">Local Government Areas</h2>
-          {lgasLoading || lgasRedLoading ? (
+          {!sortedLGAs || !scarcityLevels ? (
             <div
               className="flex flex-1 items-center justify-center"
               role="status"
@@ -180,23 +220,33 @@ const Map = ({
               <span className="sr-only">Loading...</span>
             </div>
           ) : (
-            sidebarLGAs.map(({ name, active, highDemand }) => (
+            sortedLGAs.features.map((lga, i) => (
               <div
-                key={name}
+                key={lga.properties.councilname}
                 tw="flex flex-col p-4 rounded cursor-pointer select-none hover:bg-gray-50"
-                css={{ ...(active && tw`bg-violet-50 hover:bg-violet-100`) }}
+                css={{
+                  ...(selectedCouncil === i &&
+                    tw`bg-violet-50 hover:bg-violet-100`),
+                }}
               >
                 <p tw="flex items-center font-medium gap-1">
-                  {highDemand && (
+                  {i < LGAsNeedsNotMet.features!.length && (
                     <ExclamationCircleIcon
                       strokeWidth={2}
                       tw="w-4 h-4 text-red-600"
                     />
                   )}{" "}
-                  {name}, NSW
+                  {lga.properties.councilname}, NSW
                 </p>
-                <p>TODO</p>
-                <p css={{ ...(highDemand && tw`text-red-600`) }}>TODO</p>
+                <p>Score: {scarcityLevels[lga.properties.councilname]}</p>
+                <p
+                  css={{
+                    ...(i < LGAsNeedsNotMet.features!.length &&
+                      tw`text-red-600`),
+                  }}
+                >
+                  In need
+                </p>
               </div>
             ))
           )}
@@ -213,20 +263,26 @@ const Map = ({
           onClick={togglePopup}
           interactiveLayerIds={["lgas-layer", "lgas-layer-red"]}
         >
-          {LGAs && (
-            <Source type="geojson" data={LGAs as GeoJSONSourceRaw["data"]}>
+          {LGAsNeedsMet && (
+            <Source
+              type="geojson"
+              data={LGAsNeedsMet as GeoJSONSourceRaw["data"]}
+            >
               <Layer
                 id="lgas-layer"
                 type="fill"
                 paint={{
                   "fill-color": "rgba(156, 163, 175, 15%)",
-                  "fill-outline-color": theme`colors.violet.500`,
+                  "fill-outline-color": theme`colors.green.500`,
                 }}
               />
             </Source>
           )}
-          {LGAsRed && (
-            <Source type="geojson" data={LGAsRed as GeoJSONSourceRaw["data"]}>
+          {LGAsNeedsNotMet && (
+            <Source
+              type="geojson"
+              data={LGAsNeedsNotMet as GeoJSONSourceRaw["data"]}
+            >
               <Layer
                 id="lgas-layer-red"
                 type="fill"

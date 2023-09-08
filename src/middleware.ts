baacca29bcadeb3f61e42a0815ba471854b1768b
Michael Vo <zax@zaxu.xyz>
2023-09-08 11:57:30 +1000

    feat: redirect auth page to map if authenticated in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,8 +3,15 @@ import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
   const url = request.nextUrl.clone();
-  if (url.pathname === "/") {
-    url.pathname = "/auth";
-    return NextResponse.redirect(url);
+  switch (url.pathname) {
+    case "/auth":
+      if (request.cookies.get("authenticated")?.value === "true") {
+        url.pathname = "/map";
+        return NextResponse.redirect(url);
+      }
+      break;
+    case "/":
+      url.pathname = "/auth";
+      return NextResponse.redirect(url);
   }
 }
